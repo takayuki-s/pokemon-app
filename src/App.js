3b889151e15eb6c53d79d1c85c75d3fe8482becd
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-03 09:03:47 +0900

    poleapiからポケモンのデータを取得

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from "./utils/pokemon.js"
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon"
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL)
+      console.log(res)
+    }
+    fetchPokemonData()
+  }, [])
   return (
     <div className="App">
     </div>
