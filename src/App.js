203d8869d488f0a9e1be056cde6ecf96ab9a96a0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-03 09:04:41 +0900

    データ読み込み時にローディング

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from "./utils/pokemon.js"
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon"
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL)
       console.log(res)
+      setLoading(false)
     }
     fetchPokemonData()
   }, [])
   return (
     <div className="App">
+      {loading ? (
+        <h1>ロード中…</h1>
+      ) : (
+        <h1>ポケモンデータを取得しました</h1>
+      )}
     </div>
   );
 }
