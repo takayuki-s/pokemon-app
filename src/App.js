2ffd0f10f8f7bc0455463f2904e6e78e8f74bf9e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-03 09:10:42 +0900

    ポケモンの詳細データの取得準備

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from "./utils/pokemon.js"
+import { getAllPokemon, getPokemon } from "./utils/pokemon.js"
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon"
@@ -10,11 +10,22 @@ function App() {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL)
-      console.log(res)
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results)
+      // console.log(res.results)
       setLoading(false)
     }
     fetchPokemonData()
   }, [])
+  const loadPokemon = (data) => {
+    let _pokemonData = Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon)
+        let pokemonRecord = getPokemon(pokemon.url)
+        return pokemonRecord
+      })
+    )
+  }
   return (
     <div className="App">
       {loading ? (
