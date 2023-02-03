f07e8d7ba2a5695c0be85290f13482abe8d20cad
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-04 07:53:14 +0900

    Cardコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon, getPokemon } from "./utils/pokemon.js"
+import Card from './components/Card/Card';
+import { getAllPokemon, getPokemon } from './utils/pokemon.js';
 
 function App() {
-  const initialURL = "https://pokeapi.co/api/v2/pokemon"
-  const [loading, setLoading] = useState(true)
-  const [pokemonData, setPokemonData] = useState([])
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
-      let res = await getAllPokemon(initialURL)
+      let res = await getAllPokemon(initialURL);
       // 各ポケモンの詳細なデータを取得
-      loadPokemon(res.results)
+      loadPokemon(res.results);
       // console.log(res.results)
-      setLoading(false)
-    }
-    fetchPokemonData()
-  }, [])
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
   const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(
       data.map((pokemon) => {
         // console.log(pokemon)
-        let pokemonRecord = getPokemon(pokemon.url)
-        return pokemonRecord
-      })
-    )
-    setPokemonData(_pokemonData)
-  }
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      }),
+    );
+    setPokemonData(_pokemonData);
+  };
 
-  console.log(pokemonData)
+  console.log(pokemonData);
 
   return (
     <div className="App">
       {loading ? (
         <h1>ロード中…</h1>
       ) : (
-        <h1>ポケモンデータを取得しました</h1>
+        <>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+        </>
       )}
     </div>
   );
