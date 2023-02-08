dc6f26c29abf44f9994e03c70aa9821341282168
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-02-08 19:44:45 +0900

    次のページのポケモンを表示するボタン

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState('');
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -16,6 +17,7 @@ function App() {
       // 各ポケモンの詳細なデータを取得
       loadPokemon(res.results);
       // console.log(res.results)
+      setNextURL(res.next);
       setLoading(false);
     };
     fetchPokemonData();
@@ -31,7 +33,17 @@ function App() {
     setPokemonData(_pokemonData);
   };
 
-  console.log(pokemonData);
+  // console.log(pokemonData);
+
+  const handlePrevPage = () => {};
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    // console.log(data);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setLoading(false);
+  };
 
   return (
     <>
@@ -45,6 +57,10 @@ function App() {
               {pokemonData.map((pokemon, i) => {
                 return <Card key={i} pokemon={pokemon} />;
               })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePrevPage}>前へ</button>
+              <button onClick={handleNextPage}>次へ</button>
             </div>
           </>
         )}
